0bc86f473f746f4cd5f735160a8d7eec5d2cc302
captivating-mapping-7374 <mxharris1094@yahoo.com>
2019-12-11 03:21:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,13 +4,13 @@ def addition(num1, num2)
 sum = num1 + num2
 puts sum
 end
-addtion (5,4)
+
 
 def subtraction(num1, num2)
  difference = num1 - num2
 puts difference
 end
-subtraction(10,5)
+
 
 def division(num1, num2)
 quotient = num1 / num2
@@ -29,3 +29,8 @@ end
 def square_root(num)
 puts #{cmath.square_root(num)
 end
+
+addition (5,4)
+subtraction(10,5)
+division(50,2)
+multiplication()
