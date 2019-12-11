d16d320596f582696b9599cf3bb7520246d41ec2
captivating-mapping-7374 <mxharris1094@yahoo.com>
2019-12-11 03:35:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,24 +1,19 @@
 
-def addition(num1, num2)
+def addition(num1,num2)
  num1 + num2
-
 end
-
 
 def subtraction(num1, num2)
  num1 - num2
-
 end
 
 
 def division(num1, num2)
 num1 / num2
-
 end
 
 def multiplication(num1, num2)
  num1 * num2
-
 end
 
 def modulo(num1, num2)
@@ -29,7 +24,7 @@ def square_root(num)
  Math.sqrt(num)
 end
 
-addition ('5', '4')
+addition (5,4)
 subtraction(10, 5)
 division(50, 2)
 multiplication(4, 30)
