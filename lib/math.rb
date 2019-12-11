5110f6028c3769bd63fc5a111d0603c35e016e85
captivating-mapping-7374 <mxharris1094@yahoo.com>
2019-12-11 03:31:16 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,32 +1,32 @@
 
 def addition(num1, num2)
 sum = num1 + num2
-puts sum
+
 end
 
 
 def subtraction(num1, num2)
  difference = num1 - num2
-puts difference
+
 end
 
 
 def division(num1, num2)
 quotient = num1 / num2
-puts quoutient
+
 end
 
 def multiplication(num1, num2)
 product = num1 * num2
-puts product
+
 end
 
 def modulo(num1, num2)
-puts num1.remainder(num2)
+ num1.remainder(num2)
 end
 
 def square_root(num)
-puts Math.sqrt(num)
+ Math.sqrt(num)
 end
 
 addition (5, 4)
