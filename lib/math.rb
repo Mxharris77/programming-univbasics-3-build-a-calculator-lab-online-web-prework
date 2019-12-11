2cef31e67cb53b6aeabb8d390555ff79129e0a75
captivating-mapping-7374 <mxharris1094@yahoo.com>
2019-12-11 03:20:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,11 +4,13 @@ def addition(num1, num2)
 sum = num1 + num2
 puts sum
 end
+addtion (5,4)
 
 def subtraction(num1, num2)
  difference = num1 - num2
-puts sum
+puts difference
 end
+subtraction(10,5)
 
 def division(num1, num2)
 quotient = num1 / num2
